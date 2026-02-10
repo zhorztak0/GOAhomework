02ebec6f2ca56dfbb6a251f3db035d0b270c0234
zhorztak0 <zhorztak@gmail.com>
2026-02-10 18:13:21 +0400

    second commit

--- Day 00/lesson 1.py
+++ Day 00/lesson 1.py
@@ -135,3 +135,6 @@ exitonclick()
 
 
 
+
+
+
---
+++ Day 01/Homework/index.py
@@ -0,0 +1,26 @@
+#ჩემს შესახებ
+
+print('tako zhorzholiani')
+print('i am 18 years old')
+print('i am interested in python')
+print('მყავს ცყუპისცალი და ორი ძმა')
+
+
+#საინტერესო ფაქტები :)
+
+print('A snail can sleep for three years')
+print('Like fingerprints everyones tongue print is different')
+print('Bubblegum flavored broccoli was once made by McDonalds')
+print('oranges were not always orange')
+print('In 1971, astronaut Alan Shepard played golf on the moon')
+print('Sir Isaac Newton invented the color wheel in 1666')
+print('Australia is wider than the moon')
+print('Venus is the only planet to spin clockwise')
+print('The longest English word is 189,819 letters long')
+print('The tiny pocket in jeans was designed to store pocket watches')
+print('“The Terminator” script was sold for $1')
+print('The worlds longest concert lasted 453 hours')
+print('Frida Kahlo painted 55 self-portraits')
+print('Animals can also be allergic to humans')
+print('A baby kangaroo is called a joey')
+print('The Sun makes a sound but we can not hear it')
