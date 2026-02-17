096356ab8a817d14eeafbb25849b2469fb5a5eda
zhorztak0 <zhorztak@gmail.com>
2026-02-17 22:02:05 +0400

    first commit

---
+++ Day 02/Homework/index.py
@@ -0,0 +1,61 @@
+#string-სტრინგი არის ფრჩხილებში ჩასმული ტექსტი
+#integer-ინტეჯერი არის მთელი რიცხვი,რომელიც იწერება ფრჩხილების გარეშე
+#float-ათწილადი რიცხვი
+
+
+#ცვლადი არის სახელი,რომელსაც ვანიჭებთ მნიშვნელობას.გვჭირდება ინფორმაციის შესანახად.
+
+
+
+print('GOA')
+print('programming')
+print('python')
+print('hello')
+print('world')
+
+print(2)
+print(8)
+print(7)
+print(30)
+print(16)
+
+print(3.14)
+print(100.2)
+print(8.88)
+print(7.7)
+print(10.2)
+
+
+
+name = 'Tako'
+age = 18
+country = 'Georgia'
+city = 'Tbilisi'
+movie = 'Avatar'
+
+print('hello, my name is', name , 'i am',age, 'years old' , 'i am from' ,country, 'and i live in', city, 'my favourite film is', movie)
+
+
+
+print(5+3)
+print(10+20)
+print(6+4)
+
+print(10-4)
+print(30-10)
+print(50-10)
+
+print(5*2)
+print(7*7)
+print(60*2)
+
+print(50/2)
+print(30/2)
+print(6/2)
+
+
+
+num1 = 10
+num2 = 20
+num3 = 5
+print(num1+num2+num3)
