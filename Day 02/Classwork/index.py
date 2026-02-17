5106034e4d676fff37257e7046dcd71b6a689156
zhorztak0 <zhorztak@gmail.com>
2026-02-17 19:40:49 +0400

    second commit

---
+++ Day 02/Classwork/index.py
@@ -0,0 +1,16 @@
+print(5+5)
+print(20-10)
+print(5*5)
+print(10 / 2)
+
+name = 'Tako'
+
+
+age = 18
+
+
+colour = 'blue'
+
+
+
+print('my name is', name, "my age is ", age, "my fav colour is ", colour)
