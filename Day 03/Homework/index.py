236510398a103269368bca527b06f6a9c0237284
zhorztak0 <zhorztak@gmail.com>
2026-02-25 14:49:28 +0400

    first commit

---
+++ Day 03/Homework/index.py
@@ -0,0 +1,87 @@
+# 2) კომენტარების სახით ახსენით რა არის "კომენტარი", როგორ უნდა შევქმნათ და რა დანიშულება გააჩნია.
+
+# 3) კომენტარების სახით ახსენით რა არის case-sensitive და case-insensetive პროგრამირების ენები.
+
+# 4) კომენტარებით ახსენით რა არის snake_case-ი და შექმენით მისი გამოყენებით რამდენიმე ცვლადი შესაბამისი მნიშვნელობებით. შეეცადეთ ააწყოთ ასეთი ცვლადებით ერთი დიდი წინადადება >3
+
+# 5) კომენტარებით ახსენით რა არის bug და debugging-ი.
+
+# 6) გამოასწორეთ შეცდომები შემდეგ კოდში:
+
+# 1. name = "Lika"
+#    print(namme)
+
+# 2. number = 5
+#    text = " apples"
+#    result = number + text
+#    print(result)
+
+# 3. 
+#    name = "liKa"
+#    name2 = name + "4"
+#    print(name2)
+
+# 4. first_user = "Lika"
+#    2nduser = "Giorgi"
+#    print(first user)
+
+# 5. first name = Data
+#    last % name = "random"
+
+# 6. first_user = "Lika"
+#    2nduser = "Giorgi"
+#    print(first user)
+
+
+
+
+
+
+# 2) კომენტარის დახმარებით ჩვენ შეგვიძლია დავწეროთ რამე წინადადება ან ავხსნათ კოდი,რომელსაც არ კითხულობს კომპიუტერი.
+# 3) case_sensitive არის პროგრამირების ენა,რომლისთვისაც დიდი მნიშვნელობა აქვს თუ როგორ ვწერთ კოდს.case_insensitive, კი პირიქით.
+# 4) snake_case არის მაგალითად ცვლადში ორი სიტყვის ერთად დაწერა.
+
+
+my_name = 'tako'
+favourite_colour = 'Blue'
+favourite_food = 'potatoes'
+
+print('my name is ' + my_name + ' i like colour ' + favourite_colour + ' and i love ' +  favourite_food )
+
+
+
+# 5) bug არის შეცდომა ხოლო debugging არის შეცდომის გამოსწორება
+
+
+# 6) გამოასწორეთ შეცდომები შემდეგ კოდში:
+
+
+# 1.
+name = "Lika"
+print(name)
+
+# 2.
+number = 5
+text = "apples"
+result = str(number) + text
+print(result)
+
+# 3.
+name = 'liKa'
+name2 = name + "4"
+print(name2)
+
+# 4. 
+first_user = "Lika"
+second_user = "Giorgi"
+print(first_user)
+
+# 5.
+first_name = 'Data'
+last_name = "random"
+print(last_name)
+
+# 6.
+first_user = "Lika"
+second_user = "Giorgi"
+print(first_user)
