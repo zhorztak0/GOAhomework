cd1c2aa1a6d1d044fd7e997b6a5a583f7f70a565
zhorztak0 <zhorztak@gmail.com>
2026-04-03 16:16:35 +0400

    first commit

---
+++ Day 08/Classwork/index.py
@@ -0,0 +1,37 @@
+#1) კომენტარების სახით ახსენით რა არის logical operators, ჩამოწერეთ თითოეული და განმარტეთ რომელი რა დროს რა შედეგს გვიბრუნებს.
+
+#2) გააკეთეთ ყველა შედარებით ოპერატორებზე 3 მაგალითი.
+
+#3) გააკეთეთ 2 მაგალითი logical operator-ებზე.
+
+
+# logical operators არის and და or.თუ (true and true) მაშინ პასუხი თრუ არის. 
+print(True and True) # გამოდის true
+print(True and False)# გამოდის false
+print(False and False)#გამოდის false
+
+print(True or True)#გამოდის true
+print(True or False)#გამოდის true
+print(False or False)#გამოდის false
+  
+
+print(5 > 2)# true
+print(8 > 9)# false
+print(7 > 4)# true
+
+print(7 < 2)#false
+print(10 < 14)#True
+print(5 < 4)# false
+
+print(5 == 5)#true
+print(7 == 2)#false
+print(10 == 10)#true
+
+
+print(True and False)
+print(True and True)
+
+print(True or False)
+print(False or False)
+
+
